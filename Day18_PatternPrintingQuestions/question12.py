a76f61a235cf091026a8dd7363ddbdf868614fa5
Geetika Vishwakarma <geetikavishwakarma808@gmail.com>
2026-08-19 23:54:39 +0530

    Day18- Pattern Printing Questions

---
+++ Day18_PatternPrintingQuestions/question12.py
@@ -0,0 +1,17 @@
+#Right-Aligned Number Triangle
+'''
+    1
+   12
+  123
+ 1234
+12345
+'''
+n = 5
+
+for i in range(1, n + 1):
+    print(" " * (n - i), end="")
+
+    for j in range(1, i + 1):
+        print(j, end="")
+
+    print()
