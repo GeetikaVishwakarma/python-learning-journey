be945d6a8083a40e7971b62a6020c613281e285d
Geetika Vishwakarma <geetikavishwakarma808@gmail.com>
2026-08-24 23:53:07 +0530

    Pattern Printing

---
+++ Day18_PatternPrintingQuestions/question15.py
@@ -0,0 +1,18 @@
+#Repeated Number Pyramid
+'''
+    1
+   2 2
+  3 3 3
+ 4 4 4 4
+5 5 5 5 5
+'''
+
+n = 5
+
+for i in range(1, n + 1):
+    print(" " * (n - i), end="")
+
+    for j in range(i):
+        print(i, end=" ")
+
+    print()
